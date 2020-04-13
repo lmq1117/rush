b8b079cee80f8ca2aae2b782d0ed889c52f4cdf5
limq <limq@cgnfs.com.cn>
2020-04-13 09:37:22 +0800

    hello world

---
+++ src/test/java/com/buy/rush/RushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buy.rush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
